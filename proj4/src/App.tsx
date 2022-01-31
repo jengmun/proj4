625d4f79c9e40949a8efa9e45f0372696d11677f
jengmunw <wongjm4@hotmail.com>
2022-01-31 17:27:16 +0800

    basic setup

--- proj4/src/App.tsx
+++ proj4/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./pages/Home/Home";
+import Overview from "./pages/Shop/Overview";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Cart from "./pages/Cart/Cart";
+import CheckOut from "./pages/CheckOut/CheckOut";
+import CreateAccount from "./pages/CreateAccount/CreateAccount";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Overview} />
+        <Route path="/shop/:id" component={ProductDetails} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/checkout" component={CheckOut} />
+        <Route path="/createaccount" component={CreateAccount} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
---
+++ proj4/src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Cart</li>
+        <li>Account</li>
+        <li>Login/Logout</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
